591441740a7672bd72550a2b2899c08cdbfc597b
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-09-05 10:57:07 -0400

    new balloon code

--- src/Monitors/ButtonMonitor.cpp
+++
@@ -1,7 +0,0 @@
-#include "ButtonMonitor.hpp"
-
-ButtonMonitor::ButtonMonitor(unsigned int offset): TimedControlTask<void>(offset){}
-
-void ButtonMonitor::execute(){
-    sfr::button::pressed = digitalRead(constants::button::button_pin); 
-}
